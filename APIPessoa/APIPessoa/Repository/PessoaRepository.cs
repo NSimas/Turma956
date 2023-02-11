474c537cb8cee8108bb39cb83df1010e1615d179
Nélia Simas <neliasimas@hotmail.com>
2023-02-11 13:58:21 -0300

    adds

--- APIPessoa/APIPessoa/Repository/PessoaRepository.cs
+++ APIPessoa/APIPessoa/Repository/PessoaRepository.cs
@@ -1,4 +1,6 @@
-﻿using Dapper;
+﻿using Microsoft.AspNetCore.Components.Routing;
+using MySqlConnector;
+using Dapper;
 using MySqlConnector;
 
 namespace APIPessoa.Repository
@@ -14,6 +16,17 @@ namespace APIPessoa.Repository
             using MySqlConnection conn = new(stringConnection);
 
             return conn.Query<Pessoa>(query).ToList();
+        }
+        public void InserirPessoas(Pessoa pessoa)
+        {
+            string query = $"INSERT INTO Pessoa(Nome, DataNascimento, QuantidadeFilhos) " +
+                $"VALUES ('{pessoa.Nome}','{pessoa.DataNascimento.ToString("yyyy-MM-dd")}'," +
+                $"{pessoa.QuantidadeFilhos})";
+            string stringConnection = Environment.GetEnvironmentVariable("DATABASE_CONFIG");
+
+            using MySqlConnection conn = new(stringConnection);
+
+            conn.Query(query);
         }
     }
 }
